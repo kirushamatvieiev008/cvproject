4a0b81eac266015ae83816c02d2f9abf91fe8a17
kirushamatvieiev008 <mkiril087@gmail.com>
2026-03-06 21:36:29 +0200

    finished2.0

--- src/app.js
+++ src/app.js
@@ -1,112 +1,59 @@
 import './style.scss';
 
-import { Notyf } from 'notyf';
-import 'notyf/notyf.min.css';
-
 import Swal from 'sweetalert2'
 
-const notyf = new Notyf();
+const btns = document.querySelectorAll(".buyTicketBtns");
 
-const btn = document.querySelectorAll(".buyTicketBtns");
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
 
-document.body.addEventListener('click', event => {
-    if (event.target.classList[0] === 'buyTicketBtns') {
-        event.target.addEventListener("click", () => {
+        Swal.fire({
+            title: "Замовлення квитка",
 
-            Swal.fire({
-                title: "Замовлення квитка",
-
-                html: `
+            html: `
       <input id="swal-name" class="swal2-input" placeholder="Ваше ім'я">
       <input id="swal-email" class="swal2-input" type="email" placeholder="Email">
       <input id="swal-count" class="swal2-input" type="number" placeholder="Кількість квитків" min="1">
     `,
 
-                background: "#242833",
-                color: "#ffffff",
+            background: "#242833",
+            color: "#ffffff",
 
-                confirmButtonText: "Купити",
-                cancelButtonText: "Скасувати",
+            confirmButtonText: "Купити",
+            cancelButtonText: "Скасувати",
 
-                showCancelButton: true,
+            showCancelButton: true,
 
-                preConfirm: () => {
-                    return {
-                        name: document.getElementById("swal-name").value,
-                        email: document.getElementById("swal-email").value,
-                        count: document.getElementById("swal-count").value
-                    }
+            preConfirm: () => {
+                return {
+                    name: document.getElementById("swal-name").value,
+                    email: document.getElementById("swal-email").value,
+                    count: document.getElementById("swal-count").value
                 }
+            }
 
-            }).then((result) => {
+        }).then((result) => {
 
-                if (result.isConfirmed) {
+            if (result.isConfirmed) {
 
-                    const data = result.value;
+                const data = result.value;
 
-                    Swal.fire({
-                        title: "Успіх 🎉",
-                        text: `Дякуємо ${data.name}! Ви замовили ${data.count} квиток(и).`,
-                        icon: "success",
-                        background: "#242833",
-                        color: "#fff"
-                    });
+                Swal.fire({
+                    title: "Успіх 🎉",
+                    text: `Дякуємо ${data.name}! Ви замовили ${data.count} квиток(и).`,
+                    icon: "success",
+                    background: "#242833",
+                    color: "#fff"
+                });
 
-                }
-
-            });
+            }
 
         });
 
-    }
-})
+    });
 
-// btn.addEventListener("click", () => {
-
-//     Swal.fire({
-//         title: "Замовлення квитка",
-
-//         html: `
-//       <input id="swal-name" class="swal2-input" placeholder="Ваше ім'я">
-//       <input id="swal-email" class="swal2-input" type="email" placeholder="Email">
-//       <input id="swal-count" class="swal2-input" type="number" placeholder="Кількість квитків" min="1">
-//     `,
-
-//         background: "#242833",
-//         color: "#ffffff",
-
-//         confirmButtonText: "Купити",
-//         cancelButtonText: "Скасувати",
-
-//         showCancelButton: true,
-
-//         preConfirm: () => {
-//             return {
-//                 name: document.getElementById("swal-name").value,
-//                 email: document.getElementById("swal-email").value,
-//                 count: document.getElementById("swal-count").value
-//             }
-//         }
-
-//     }).then((result) => {
-
-//         if (result.isConfirmed) {
-
-//             const data = result.value;
-
-//             Swal.fire({
-//                 title: "Успіх 🎉",
-//                 text: `Дякуємо ${data.name}! Ви замовили ${data.count} квиток(и).`,
-//                 icon: "success",
-//                 background: "#242833",
-//                 color: "#fff"
-//             });
-
-//         }
-
-//     });
-
-// });
+}
+)
 
 const contactsForm = document.querySelector('.contactsForm');
 
@@ -121,10 +68,6 @@ contactsForm.addEventListener('submit', event => {
     if (inpName.value && inpGmail.value.includes('@') && inpGmail.value.includes('.')) {
         console.log(inpName, inpGmail, message);
 
-        // if () {
-
-        // }
-
         let toStorage = [];
         toStorage.push({
             name: inpName.value,
@@ -133,16 +76,9 @@ contactsForm.addEventListener('submit', event => {
         })
         localStorage.setItem(`userId${localStorage.length}`, JSON.stringify(toStorage));
 
-
-
         inpName.value = '';
         inpGmail.value = '';
         message.value = '';
     }
 
 });
-
-// console.log(JSON.parse(localStorage.getItem('кірілматвєєвo')));
-
-
-// console.log(localStorage.key());
